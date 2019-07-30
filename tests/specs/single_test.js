9d09bf0d45284fd38f97ef542af94953e2015693
C_Karadkar <c.karadkar@gmail.com>
2019-07-30 22:38:43 +0530

    Updated

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 
 describe("Google's Search Functionality", function() {
-  it("can find search results", function() {
+  it("Open Google", function() {
     browser.url("https://www.google.com");
   });
 });
