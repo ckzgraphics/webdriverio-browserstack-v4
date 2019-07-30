8d5af2a6a409ec6944ccf2c5c2ea5a6c032b6d2b
chimi <ckzgraphics@users.noreply.github.com>
2019-07-30 15:07:22 +0530

    Create new single test.js

---
+++ tests/specs/new single test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+
+describe('Open Facebook Site', function() {
+  it('can find search results', function () {
+    browser
+      .url('https://www.facebook.com'); 
+  });
+});
