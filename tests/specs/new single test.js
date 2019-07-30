9be605775487eb1c916c18c89fffef21f634567e
chimi <ckzgraphics@users.noreply.github.com>
2019-07-30 15:12:55 +0530

    Update new single test.js

--- tests/specs/new single test.js
+++ tests/specs/new single test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-describe('Open Facebook Site', function() {
+describe('Open.Facebook Site', function() {
   it('can find search results', function () {
     browser
       .url('https://www.facebook.com'); 
