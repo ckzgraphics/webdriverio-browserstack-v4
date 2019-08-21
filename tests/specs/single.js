949cc5ef931288cf6d046913810f88a534f4c91a
C_Karadkar <c.karadkar@gmail.com>
2019-08-21 16:06:30 +0530

    added

--- tests/specs/single.js
+++ tests/specs/single.js
@@ -4,7 +4,9 @@ describe("describe BStack", function() {
   it("Open BrowserStack", function() {
     browser.url("https://www.browserstack.com");
   });
+});
 
+describe("describe Facebook", function() {
   it("Open Facebook", function() {
     browser.url("https://www.facebook.com");
   });
